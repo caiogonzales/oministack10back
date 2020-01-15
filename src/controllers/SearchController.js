6b389a04ec454db6dc759b3d966b6100d8f8978a
Caio Cesar Monteiro Gonzales <cmgonzales@msxint.com>
2020-01-15 16:38:44 -0300

    backend da app

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,29 @@
+// buscar devs num raio de 10km
+// filtar por techs 
+const Dev = require('../models/Dev')
+const parseStringAsArray = require('../utils/parseStringAsArray')
+
+module.exports = {
+    async index(req, res){
+     
+        const { latitude, longitude, techs } = req.query;
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 10000,
+                },
+            },
+        });
+
+      return res.json({ devs })
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes')
+const app = express();
+
+mongoose.connect('mongodb://localhost/week10', { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(()=>{ console.log('connectado ao banco')})
+        .catch((err)=>{ console.log("erro ao connectar"+err) })
+
+
+app.use(express.json());
+app.use(routes)
+// métodos HTTP: GET, POST, PUT, DELETE
+
+// tipos de parametros:
+// Query Params: req.query(filtros, ordenação, paginação)
+// Route Params: req.params (identiciar um recurso na alteracao ou remocao)
+// Body: request.body (Dados para criação ou alteração de um registro  )
+
+
+
+
+app.listen(3333);
